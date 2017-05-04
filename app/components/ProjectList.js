7dccf844b00c36d58a5057fbd36a8c08648fa142
smchow <sunita.m.chowdhury@gmail.com>
2017-05-04 18:13:22 -0400

    Added button to show data

--- app/components/ProjectList.js
+++ app/components/ProjectList.js
@@ -4,20 +4,31 @@ import {
    View,
    ListView,
    Text,
+   Button,
+   Image,
    StyleSheet
 } from 'react-native'
 
 export default ProjectList = (props) => {
    return (
       <View>
+         <Text style = {styles.headerItem}> My Projects </Text>
          <ListView
             style = {styles.listContainer}
             dataSource = {props.dataSource}
             renderRow = {
                (rowData) => (
+                  <View style={styles.container}>
                   <Text style = {styles.listItem}>
-                     {rowData.name}
-                  </Text>
+                     {rowData.name} {"\n"}
+                     Description: {rowData.tagLine}
+            {/*rowData.image_url*/} 
+                      </Text>
+
+                      <Button title="View Data"
+                        onPress={() => this._handleData(rowData.id)}
+                      />
+                     </View>
                )
             }
          />
@@ -30,7 +41,15 @@ const styles = StyleSheet.create ({
       paddingTop: 22
    },
    listItem: {
+      paddingBottom: 22,
       fontSize: 30,
+      fontWeight: 'bold',
+      textAlign: 'left',
+
+   },
+   headerItem: {
+      fontSize: 50,
+      marginTop: 50,
       fontWeight: 'bold',
       textAlign: 'center',
    }
